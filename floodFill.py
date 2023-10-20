08f520faab06c7d7a2a34f267636dcc8bb01e5bf
aryawardhana <wardhana2451@gmail.com>
2023-10-20 15:59:48 +0700

    floodfill

--- floodFill.py
+++ floodFill.py
@@ -2,6 +2,7 @@ import os
 import time
 import random
 
+# Representasi layout dalam bentuk string
 layout = \
     """
             _____
@@ -24,14 +25,15 @@ layout = \
 
     """.split('\n')
 
+# Memecah layout menjadi list karakter
 layout = [
     list(row) for row in layout
 ]
 
-# bounding chars of the hexagon
+# Karakter pembatas dari heksagon
 bound_chars = ['\\', '/', '\'', '_', '-', '.', ',', '`', '(', ')']
 
-# points within each of the hexagon
+# Titik awal dalam setiap heksagon
 points = [
     (4, 14),
     (6, 6),
@@ -42,49 +44,47 @@ points = [
     (14, 14),
 ]
 
-
+# Fungsi untuk mencetak layout heksagon
 def print_layout():
     # os.system('clear')
     print("\n".join(["".join(row) for row in layout]))
 
-
+# Fungsi rekursif untuk flood fill
 def _flood(x, y):
-    # check for the bounds
+    # Cek batas layout
     if x >= len(layout) or x < 0 or y < 0 or y >= len(layout[x]):
         return
 
-    # check for the bounds within each hexagon
+    # Cek batas dalam heksagon
     if layout[x][y] in bound_chars:
         return
 
-    # check if already filled
+    # Cek apakah sudah diisi
     if layout[x][y] == '#':
         return
 
-    # fix the position
+    # Isi posisi saat ini
     layout[x][y] = '#'
 
-    # add sleep
+    # Tambahkan jeda
     time.sleep(0.01)
 
-    # render the layout
+    # Cetak layout yang telah diubah
     print_layout()
 
-    # recursively invoke the function
-    # the core of the algorithm
+    # Rekursif memanggil fungsi flood fill untuk tetangga
     _flood(x + 1, y)
     _flood(x - 1, y)
     _flood(x, y + 1)
     _flood(x, y - 1)
 
-
+# Fungsi utama untuk memulai flood fill dari titik awal
 def flood(x, y):
     _flood(x, y)
 
-
-# randomly shuffle the points
+# Acak urutan titik awal flood fill
 random.shuffle(points)
 
-# do flood fill for each hexagon
+# Lakukan flood fill untuk setiap heksagon
 for X, Y in points:
     flood(X, Y)
